8250d9fc0a85632de8cb2a2b86da86852bb5e75e
Nate <88175124+NateMBK@users.noreply.github.com>
2024-07-24 02:09:56 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -3,10 +3,8 @@
 # Define log file
 LOGFILE="$(dirname "$0")/install_log.txt"
 
-# Function to log messages
-log() {
-    echo "$(date): $1" | tee -a $LOGFILE
-}
+# Redirect all output to log file
+exec > >(tee -a "$LOGFILE") 2>&1
 
 # Update system packages
 echo "Updating system packages..."
@@ -62,38 +60,36 @@ packages=(
 
 # Install packages
 for package in "${packages[@]}"; do
-  log "Installing $package..."
-  dnf install -y "$package" | tee -a $LOGFILE
+  dnf install -y "$package"
 done
 
 # Detect Nvidia GPU
-log "Detecting Nvidia GPU..."
+echo "Detecting Nvidia GPU..."
 GPU_ID=$(lspci | grep -i nvidia | cut -d ' ' -f 1)
 AUDIO_ID=$(lspci | grep -i nvidia | grep -i audio | cut -d ' ' -f 1)
 GPU_IDS="$GPU_ID $AUDIO_ID"
 
 # Disable nouveau driver
-log "Disabling nouveau driver..."
+echo "Disabling nouveau driver..."
 echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
 mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
 dracut /boot/initramfs-$(uname -r).img $(uname -r)
 
 # Enable IOMMU and VFIO for AMD
-log "Enabling IOMMU and VFIO for AMD..."
+echo "Enabling IOMMU and VFIO for AMD..."
 echo "iommu=pt iommu=1 vfio_iommu_type1.allow_unsafe_interrupts=1" >> /etc/default/grub
 grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
 echo "options vfio-pci ids=$GPU_IDS" >> /etc/modprobe.d/vfio.conf
 echo "vfio-pci" >> /etc/modules-load.d/vfio-pci.conf
 
 # Create .config directory in all users' home directories
-log "Creating .config directory in all users' home directories..."
+echo "Creating .config directory in all users' home directories..."
 for dir in /home/*; do
   if [ -d "$dir" ]; then
     mkdir -p "$dir/.config"
     chown $(basename "$dir"):$(basename "$dir") "$dir/.config"
-    log ".config directory created for user $(basename "$dir")"
+    echo ".config directory created for user $(basename "$dir")"
   fi
 done
 
-log "All packages installed successfully!"
-
+echo "All packages installed successfully!"
