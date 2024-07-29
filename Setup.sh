de3121ae7203cfaf739bedbeb5282fe687b061eb
Nate <88175124+NateMBK@users.noreply.github.com>
2024-07-28 23:00:08 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -23,11 +23,6 @@ install_packages() {
   # Install packages
   for package in "${packages[@]}"; do
     dnf install -y "$package"
-    wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
-    && cd ~/.local/share/fonts \
-    && unzip JetBrainsMono.zip \
-    && rm JetBrainsMono.zip \
-    && fc-cache -fv
   done
 }
 
