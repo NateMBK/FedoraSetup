19d52b2ab6b0fdd7a26fea8e3827d8ec748f0c2b
Nate <88175124+NateMBK@users.noreply.github.com>
2024-08-06 23:12:30 -0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-wget https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-fedora-40-$(uname -m).rpm && sudo dnf install ./sunshine-fedora-40-$(uname -m).rpm && sudo systemctl start sunshine && sudo systemctl enable sunshine
+# Enable RPM Fusion repositories
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Download the Sunshine RPM package
+wget https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-fedora-40-$(uname -m).rpm
+
+# Install the Sunshine package
+sudo dnf install -y ./sunshine-fedora-40-$(uname -m).rpm
+
+# Start and enable the Sunshine service
+sudo systemctl start sunshine
+sudo systemctl enable sunshine
+
+echo "Sunshine installation and setup complete!"
